b7f5b6aa35004ba219c4eaf94bcc8c768d289b76
Viswanath. Ct <2ndryid@gmail.com>
2019-06-01 17:47:13 +0530

    Improved the measuring helper, nthWeekDay.

--- src/parser.js
+++ src/parser.js
@@ -7,29 +7,31 @@
 const selectingConfigPrecedence = ['nthWeekDay', 'dayOfWeek', 'dateOfMonth'];
 
 /* Helpers */
+const dayOfWeek = (date) => date.getDay() + 1;
+
 const scales = {
-	dayOfWeek: (date) => date.getDay(),
+	dayOfWeek,
 	dateOfMonth: (date) => date.getDate(),
 	nthWeekDay: (date, config) => {
-		const jsWeekDay = config.dayOfWeek[0] - 1; //NOTE: When multiple dayOfWeeks are present only the first value is considered.
+		const weekDay = config.dayOfWeek[0]; //NOTE: When multiple dayOfWeeks are present only the first value is considered.
 
-		if(date.getDay() !== jsWeekDay)
+		if(scales.dayOfWeek(date) !== weekDay)
 			return;
 
 		const monthStart = new Date(date).setDate(1);
 		const firstDateWithSameWeekDay = new Date(monthStart);
-		while(firstDateWithSameWeekDay.getDay() != jsWeekDay) {
+		while(dayOfWeek(firstDateWithSameWeekDay) !== weekDay)
 			firstDateWithSameWeekDay.setDate(firstDateWithSameWeekDay.getDate() + 1);
-		}
 
 		return (date.getDate() - firstDateWithSameWeekDay.getDate()) / 7 + 1;
 	},
+	month: (date) => date.getMonth() + 1,
 }
 
 module.exports = (config) => {
 	const { startDate, endDate } = config;
 	const dateIndex = new Date(startDate);
-	const selectingConfig = selectingConfigPrecedence.filter((configName) => config[configName])[0]
+	const selectingConfig = selectingConfigPrecedence.filter((configName) => config[configName])[0];
 	const selectingScale = scales[selectingConfig];
 
 	const selectedDates = [];
